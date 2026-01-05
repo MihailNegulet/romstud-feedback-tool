522f1be8512e76570e51c6f5fb3a2b60a3d07285
Mihail <mihailnegulet@gmail.com>
2026-01-05 13:05:48 +0200

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,12 @@ export const projects = [
     { id: 2, name: "Paris: Istorie si Cultura, editia 10" },
     { id: 3, name: "La pas prin Europa: Amsterdam, editia 18" },
     { id: 4, name: "Paris: Istorie si Cultura, editia 11" },
-    { id: 5, name: "Să cucerim Nordul: Stockholm, baby!, editia 1" }
+    { id: 5, name: "Să cucerim Nordul: Stockholm, baby!, editia 1" },
+    { id: 6, name: "Revelion Atena, editia 1" },
+    { id: 7, name: "Revelion Atena, editia 2" },
+    { id: 8, name: "Revelion Atena, editia 3" },
+    { id: 9, name: "Revelion Cracovia, editia 1" },
+    { id: 10, name: "Revelion Cracovia, editia 2" }
     
 ];
+
